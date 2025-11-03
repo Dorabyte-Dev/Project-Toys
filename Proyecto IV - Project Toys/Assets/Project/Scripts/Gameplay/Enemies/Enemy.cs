e654c329c775808a3520e8c58975d4e5eb38cf3e
BerserkurNokk <97472776+BerserkurNokk21@users.noreply.github.com>
2025-11-03 19:55:48 +0100

    Rescript 20251103
    Comienzo de adaptacion de script de David a FSM

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Enemy.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,15 +1,82 @@
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEditor.VersionControl.Asset;
 
 public class Enemy : Entity
 {
     public Enemy_IdleState idleState;
     public Enemy_MoveState moveState;
 
+    [Header("Enemy Specs")]
+    public float range;
+    public float attackRange;
+    public float waitTime;
+    public float acceleration;
+
+    [Header("Enemy Attack Specs")]
+    public float attackAcceleration;
+    public int damage;
+    public float attackSpeed;
+    public GameObject damageCollider;
+    public bool isAttacking;
+
+    [Header("Player Coords")]
+    [HideInInspector] public Transform playerTransform;
+    private Vector3 lastPlayerPosition;
+    private Vector3 currentPosition;
+    private Vector3 attackPoint;
+    public bool jugadorDetectado;
+    private int nearness;
+
+    public NavMeshAgent agent;
+    private float currentTime;
+
     public int facingDirection = 1;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        agent.acceleration = acceleration;
+        damageCollider.SetActive(false);
+    }
+    protected override void Start()
+    {
+        base.Start();
+        stateMachine.Initialize(idleState);
+    }
 
     public void Flip()
     {
         facingDirection *= -1;
         transform.Rotate(0f, 180f, 0f);
     }
+
+
+    #region Player Detection
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            nearness++;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerTransform = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            nearness--;
+        }
+    }
+    #endregion
 }
--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/States/Enemy_MoveState.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/States/Enemy_MoveState.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_MoveState : EnemyState
 {
@@ -6,16 +7,42 @@ public class Enemy_MoveState : EnemyState
     {
     }
 
+    public override void Enter()
+    {
+        base.Enter();
+        GoToRandomPoint();
+    }
+
 
     public override void Update()
     {
         base.Update();
-        enemy.SetVelocity(enemy.moveSpeed * enemy.facingDirection, rb.linearVelocity.y);
 
         if (!enemy.groundDetected)
         {
             stateMachine.ChangeState(enemy.idleState);
             enemy.Flip();
         }
+    }
+
+    public void GoToRandomPoint()
+    {
+        if (enemy.agent.pathPending || !enemy.agent.isOnNavMesh || enemy.agent.remainingDistance > 0.1f)
+            return;
+
+        enemy.agent.destination = RandomNavSphere(enemy.transform.position, enemy.range, -1);
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
