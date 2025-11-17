86baab0cb6cd21bdba3a883917ec6eedba05dad6
DDPan147 <ddgarpan147@gmail.com>
2025-11-17 19:58:52 +0100

    Update Enemy_Health.cs
    Cambios que tenia que separar porque o sino daba conflicto

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Enemy_Health.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Enemy_Health.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class Enemy_Health : Entity_Health
 {
     [SerializeField] private Enemy enemy;
-
+    public EnemyUI enemyUI;
     public override void Awake()
     {
         base.Awake();
         enemy = GetComponent<Enemy>();
+        if (this.GetComponent<EnemyUI>())
+        {
+            enemyUI = GetComponent<EnemyUI>();
+        }
+        else
+        {
+            Debug.LogError("EnemyUI and Enemy Health are not in the same object");
+        }
     }
     public override void TakeDamage(float takeDamage, Transform damageDealer)
     {
         base.TakeDamage(takeDamage, damageDealer);
         //enemy.ChangeFlintState();
+        enemyUI.RecieveDamage((int)takeDamage);
         if (isDead)
             return;
     }
