2b3c2dc783cf8b94e169a9449eb032238674eeae
oATLASo <victorklaatu@gmail.com>
2025-12-05 17:12:57 +0100

    + Cambios en Main Menu
    -Añadido sistema de partículas básico. No se ejecuta cuando quiero pero weno, lo tocaré más tarde.

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Menu/BlockButton.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Menu/BlockButton.cs
@@ -1,11 +1,60 @@
 using UnityEngine;
+using System.Collections;
 
 public class BlockButton : MonoBehaviour
 {
-    public System.Action onClick;  // Lo que hará este bloque cuando se pulse
+    public System.Action onClick;
+
+    public ParticleSystem hitParticles;  // Tus partículas
+    public GameObject brokenPrefab;      // El bloque roto
+    public float shakeIntensity = 0.05f;
+    public int hitsToBreak = 3;
+
+    private int currentHits = 0;
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        originalPos = transform.localPosition;
+    }
 
     private void OnMouseDown()
     {
-        onClick?.Invoke();
+        currentHits++;
+
+        // Vibración
+        StartCoroutine(Shake());
+
+        // Partículas
+        if (hitParticles != null)
+            hitParticles.Play();
+
+        // Si se ha roto
+        if (currentHits >= hitsToBreak)
+        {
+            BreakBlock();
+        }
+        else
+        {
+            onClick?.Invoke();
+        }
+    }
+
+    private IEnumerator Shake()
+    {
+        float time = 0.1f;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            transform.localPosition = originalPos + Random.insideUnitSphere * shakeIntensity;
+            yield return null;
+        }
+        transform.localPosition = originalPos;
+    }
+
+    private void BreakBlock()
+    {
+        Instantiate(brokenPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);  // Destruye el bloque original
     }
 }
