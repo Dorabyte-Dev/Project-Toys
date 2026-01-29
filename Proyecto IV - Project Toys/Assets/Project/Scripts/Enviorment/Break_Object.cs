1fa10fa263b2f3b5bd87b54d1ae35c49c9210b12
BerserkurNokk <97472776+BerserkurNokk21@users.noreply.github.com>
2026-01-29 13:01:21 +0100

    Update 20260129
    Update break object effect, added shrink effect

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Enviorment/Break_Object.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Enviorment/Break_Object.cs
@@ -1,16 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Break_Object : MonoBehaviour
 {
     [SerializeField] private List<Rigidbody> rigidbodies = new List<Rigidbody>();
     [SerializeField] private List<MeshCollider> colliders = new List<MeshCollider>();
+    [SerializeField] private List<Transform> transforms = new List<Transform>();
 
     void Start()
     {
         // Get all rigibodys and mesh colliders of child objects
         rigidbodies.AddRange(GetComponentsInChildren<Rigidbody>());
         colliders.AddRange(GetComponentsInChildren<MeshCollider>());
+        transforms.AddRange(GetComponentsInChildren<Transform>());
 
         // Set the parameters to the child objects
         foreach (var rb in rigidbodies)
@@ -35,8 +38,25 @@ public class Break_Object : MonoBehaviour
         foreach (var col in colliders)
         {
             col.enabled = true;
-        }
+        }   
+        
+        Invoke("ShrinkEffect", 0.8f);
+    }
 
-        Destroy(gameObject, 3f);
+    private void ShrinkEffect()
+    {
+        float delayBeforePop = 0.5f;
+        float popDuration = 0.8f;
+
+        foreach (var t in transforms)
+        {
+            Sequence popSequence = DOTween.Sequence();
+
+            popSequence.AppendInterval(delayBeforePop);
+            
+            popSequence.Append(t.DOScale(t.localScale * .8f, popDuration * 0.01f));
+            popSequence.Append(t.DOScale(Vector3.zero, popDuration * 0.8f).SetEase(Ease.InBack));
+            popSequence.OnComplete(() => Destroy(gameObject));
+        }
     }
 }
