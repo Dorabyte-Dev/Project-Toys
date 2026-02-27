e74ce5e72c923607d6e4eb587af6221f2fb211c8
BerserkurNokk21 <oscesp200@gmail.com>
2026-02-27 16:12:48 +0100

    Refactor 20260227
    VFX Enemy function refactor

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Modules/Enemy_VFX.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Enemies/Modules/Enemy_VFX.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.VFX;
@@ -6,6 +7,8 @@ public class Enemy_VFX : Entity_VFX
 {
     [SerializeField] private VisualEffect pDodgeShine;
     [SerializeField] private ParticleSystem pHitEffect;
+    
+    public event Action OnDissolveComplete;
     
     public void ShineEffect()
     {
@@ -17,4 +20,40 @@ public class Enemy_VFX : Entity_VFX
         pHitEffect.transform.rotation = particleRotation;
         pHitEffect.Play();
     }
+    
+    public override void DeathVFX_Feedback()
+    {
+        base.DeathVFX_Feedback();
+        StartCoroutine(Dissolve());
+    }
+    
+    #region DissolveFeedback
+    public IEnumerator Dissolve()
+    {
+        if (vfxGraph)
+        {
+            vfxGraph.Play();
+        }
+
+        if (meshMaterials != null)
+        {
+            if (meshMaterials.Length > 0)
+            {
+                while(meshMaterials[0].GetFloat("_DissolveAmount") < 1f)
+                {
+                    for (int i = 0; i < meshMaterials.Length; i++)
+                    {
+                        float currentDissolve = meshMaterials[i].GetFloat("_DissolveAmount");
+                        meshMaterials[i].SetFloat("_DissolveAmount", currentDissolve + dissolveRate);
+                    }
+                    yield return new WaitForSeconds(refreshRate);
+                }
+            }
+        }
+        OnDissolveComplete?.Invoke();
+    }
+    
+
+    #endregion
+
 }
--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/FSM/Entity/Entity_VFX.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/FSM/Entity/Entity_VFX.cs
@@ -10,28 +10,36 @@ public class Entity_VFX : MonoBehaviour
 {
     private static readonly int Color1 = Shader.PropertyToID("_Color");
     
-    [SerializeField] private Rigidbody rb;
-    [Header("ColorChange")]
-    public Color feedbackColor;
-    public float feedbackDuration;
-    private List<Material[]> originalMaterials = new List<Material[]>();
-    private List<Color[]> originalColors = new List<Color[]>();
-    private Coroutine changeCoroutine;
-    private Coroutine revertCoroutine;
-    [FormerlySerializedAs("pushStrengh")] [Header("PushFeedback")]
-    public float pushStrength;
-    [Range(0, 1)] public float pushDuration;
-    [Header("DissolveFeedback")]
-    public Renderer renderMesh;
-    public VisualEffect vfxGraph;
-    public event Action OnDissolveComplete;
-    private Material[] meshMaterials;
+    [Header("--- Core Components ---")]
+    [SerializeField] protected Rigidbody rb;
+    [SerializeField] protected Renderer renderMesh;
+    [SerializeField] protected VisualEffect vfxGraph;
+
+    [Header("--- Damage Flash (Color Change) ---")]
+    public Color feedbackColor = Color.white;
+    public float feedbackDuration = 0.1f;
+    
+    // Datos internos para restaurar materiales
+    protected List<Material[]> originalMaterials = new List<Material[]>();
+    protected List<Color[]> originalColors = new List<Color[]>();
+    protected Coroutine changeCoroutine;
+    protected Coroutine revertCoroutine;
+
+    [Header("--- Knockback (Push Feedback) ---")]
+    [FormerlySerializedAs("pushStrengh")] 
+    public float pushStrength = 5f;
+    [Range(0, 1)] public float pushDuration = 0.2f;
+
+    [Header("--- Dissolve Effect ---")]
     public float dissolveRate = 0.0125f;
-    [Range(0.1f,0.01f)] public float refreshRate = 0.025f;
-    [Header("ShakeFeedback")]
-    [SerializeField] private float shakeDuration = 1;
-    [SerializeField] private float shakeStrength = 1;
-    [SerializeField] private float randomShake = 0.2f;
+    [Range(0.01f, 0.1f)] public float refreshRate = 0.025f;
+
+    protected Material[] meshMaterials;
+
+    [Header("--- Screen/Entity Shake ---")]
+    [SerializeField] private float shakeDuration = 0.2f;
+    [SerializeField] private float shakeStrength = 0.5f;
+    [SerializeField] private float randomShake = 0.1f;
 
     protected virtual void Awake()
     {
@@ -64,6 +72,7 @@ public class Entity_VFX : MonoBehaviour
             originalColors.Add(colors.ToArray());
         }
     }
+    
     // private void Update()
     // {
     //     if (Input.GetKeyDown(KeyCode.P))
@@ -81,9 +90,9 @@ public class Entity_VFX : MonoBehaviour
         //Shake(shakeDuration, shakeStrength);
     }
     
-    public void DeathVFX_Feedback()
+    public virtual void DeathVFX_Feedback()
     {
-        StartCoroutine(Dissolve());
+        // La base no hace nada. Cada entidad decide su efecto de muerte.
     }
 
     #region ColoredFeedback
@@ -211,34 +220,7 @@ public class Entity_VFX : MonoBehaviour
         */
     }
 
-    #region DissolveFeedback
-    public IEnumerator Dissolve()
-    {
-        if (vfxGraph)
-        {
-            vfxGraph.Play();
-        }
-
-        if (meshMaterials != null)
-        {
-            if (meshMaterials.Length > 0)
-            {
-                while(meshMaterials[0].GetFloat("_DissolveAmount") < 1f)
-                {
-                    for (int i = 0; i < meshMaterials.Length; i++)
-                    {
-                        float currentDissolve = meshMaterials[i].GetFloat("_DissolveAmount");
-                        meshMaterials[i].SetFloat("_DissolveAmount", currentDissolve + dissolveRate);
-                    }
-                    yield return new WaitForSeconds(refreshRate);
-                }
-            }
-        }
-        OnDissolveComplete?.Invoke();
-    }
     
-
-    #endregion
     private void Shake(float duration, float magnitude)
     {
         StartCoroutine(ShakeCoroutine(duration, magnitude));
