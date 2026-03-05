3fd63480c0e909b26f63b43b55404138c8cce370
Álvaro Morales Cruz <159558788+Dorabyte-Dev@users.noreply.github.com>
2026-03-05 12:03:49 +0100

    Player Sword Trail V2 Done

--- Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Player/Modules/Player_VFX.cs
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Player/Modules/Player_VFX.cs
@@ -5,16 +5,19 @@ public class Player_VFX : Entity_VFX
 {
 	[SerializeField]private VisualEffect slashEffect;
 	[SerializeField] private MeshTrail swordTrail;
+	[SerializeField] private ParticleSystem swordTrailEffect;
 
 	public void Slash()
 	{
 		//slashEffect.Play();
-		swordTrail.ToggleTrail();
+		//swordTrail.ToggleTrail();
+		swordTrailEffect.Play();
 	}
 
 	public void InterruptSlash()
 	{
 		//slashEffect.Stop();
-		swordTrail.UnToggleTrail();
+		//swordTrail.UnToggleTrail();
+		swordTrailEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
 	}
 }
