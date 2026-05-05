7717b5ea8476f4ef2a0c9f152c0dd5cf29f75824
Dorabyte-Dev <dorabyte@gmail.com>
2026-05-05 16:32:36 +0200

    New Health Bar System WIP

---
+++ Proyecto IV - Project Toys/Assets/Project/Scripts/Gameplay/Extra/MultipleSpriteFillBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultipleSpriteFillBar : MonoBehaviour
+{
+    public float value;
+    public Image[] fillBars;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    //divide, module  then divide again to get the percentage for each bar, then set the fill amount of each bar to the percentage
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
